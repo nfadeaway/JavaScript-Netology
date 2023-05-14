912365c49ff90cca907f6efbbbae51013b34eb74
nfadeaway <110091929+nfadeaway@users.noreply.github.com>
2023-05-14 14:16:21 +0500

    refactoring

--- interface-design-basics/hw1/task-3/task.js
+++ interface-design-basics/hw1/task-3/task.js
@@ -2,21 +2,23 @@ const dead = document.getElementById('dead')
 const lost = document.getElementById('lost')
 const holes = document.querySelectorAll('.hole')
 
+function resetGame(status) {
+  alert(status)
+  dead.textContent = 0
+  lost.textContent = 0
+}
+
 for (let hole of holes) {
   hole.onclick = function() {
     if (hole.className.includes('hole_has-mole')) {
       dead.textContent = +dead.textContent + 1
       if (dead.textContent == 10) {
-        alert('Победа!')
-        dead.textContent = 0
-        lost.textContent = 0
+        resetGame('Победа!')
       }
     } else {
       lost.textContent = +lost.textContent + 1
       if (lost.textContent == 5) {
-        alert('Вы проиграли!')
-        dead.textContent = 0
-        lost.textContent = 0
+        resetGame('Вы проиграли!')
       }
     }
   }
