f1cb0b0a391da771df8d222535be806537f42397
nfadeaway <110091929+nfadeaway@users.noreply.github.com>
2023-05-14 14:12:36 +0500

    refactoring

--- interface-design-basics/hw1/task-2/task.js
+++ interface-design-basics/hw1/task-2/task.js
@@ -4,14 +4,8 @@ const clickerSpeed = document.getElementById('clicker__speed')
 const originalImgWidth = document.getElementById('cookie').width
 let startTime = new Date()
 
-
 cookieImg.onclick = function() {
-  clickerCounter.textContent = +clickerCounter.textContent + 1
-  if (cookieImg.width == originalImgWidth) {
-    cookieImg.width += 20
-  } else {
-    cookieImg.width -= 20
-  }
+  cookieImg.width = ++clickerCounter.textContent % 2 ? 220 : 200;
   let endTime = new Date()
   clickerSpeed.textContent = (1 / (endTime - startTime) * 1000).toFixed(2)
   startTime = endTime
